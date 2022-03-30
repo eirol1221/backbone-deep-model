b4aeb43d15e549a2443af9f1d5b0dbe926c0a78a
thavisoukj1 <70977322+thavisoukj1@users.noreply.github.com>
2022-03-30 09:03:12 -0500

    revitalize repo

---
+++ ci.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+npm install
+
+if [[ -n "$UNDERSCORE" ]]
+then
+	npm install underscore@"$UNDERSCORE"
+fi
+
+if [[ -n "$BACKBONE" ]]
+then
+	npm install backbone@"$BACKBONE"
+fi
+
+if [[ -n "$LODASH" ]]
+then
+	npm install lodash@"$LODASH"
+	rm -Rf node_modules/underscore
+	cp -Rf node_modules/lodash node_modules/undescore
+
+	# Lodash@2 and Lodash@3 have different file structures
+	if [[ "$LODASH" < "3.0" ]]
+	then
+		rm -Rf node_modules/underscore
+		cp -Rf node_modules/lodash node_modules/undescore
+	else
+		ls node_modules
+		#mv node_modules/lodash/index.js node_modules/underscore/underscore.js
+	fi
+fi
